3cb7a894fb95ffd1f0c7190e7fccee4335710eba
nthuhuong <thuhuongnguyen160197@gmail.com>
2019-07-16 21:51:17 +0700

    add content for class Composite

--- DesignPattern/CompositePattern/Composite.cs
+++ DesignPattern/CompositePattern/Composite.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace CompositePattern
 {
-    class Composite
+    class Composite : Component
     {
+        private List<Component> _children = new List<Component>();
+        public Composite(string name)
+            : base(name)
+        {
+
+        }
+        public override void Add(Component c)
+        {
+            _children.Add(c);
+        }
+        public override void Remove(Component c)
+        {
+            _children.Remove(c);
+        }
+        public override void Display(int depth)
+        {
+            for (int i = 0; i < depth;i++ )
+            {
+                Console.WriteLine('-');
+            }
+            Console.WriteLine(name);
+            foreach (Component component in _children)
+                component.Desplay(depth + 2);
+        }
     }
 }
--- DesignPattern/CompositePattern/Leaf.cs
+++ DesignPattern/CompositePattern/Leaf.cs
@@ -8,5 +8,6 @@ namespace CompositePattern
 {
     class Leaf
     {
+
     }
 }
