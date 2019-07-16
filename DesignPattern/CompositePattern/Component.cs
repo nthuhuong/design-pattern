18d34e8b72d76dcf44d0a8e34fd667c863ef8b97
nthuhuong <thuhuongnguyen160197@gmail.com>
2019-07-16 20:36:39 +0700

    add content for class component

--- DesignPattern/CompositePattern/Component.cs
+++ DesignPattern/CompositePattern/Component.cs
@@ -8,5 +8,13 @@ namespace CompositePattern
 {
     class Component
     {
+        protected string name;
+        public Component(string name)
+        {
+            this.name = name;
+        }
+        public abstract void Add(Component c);
+        public abstract void Remove(Component c);
+        public abstract void Desplay(int depth);
     }
 }
